44add3d39a54c5ee1aa35d15d17ccb3c1bf3a32e
enoch finley <enochfinley@gmail.com>
2015-08-28 13:50:23 -0600

    added the Crus Class

---
+++ src/com/example/auctionapplicationIntermed/CrudModel.java
@@ -0,0 +1,22 @@
+package com.example.auctionapplicationIntermed;
+
+public class CrudModel {
+	private enum Command{
+		ADD, DELETE, BID, UPDATE
+	}
+	private Command command;
+	private String args;
+	
+	public CrudModel(Command cmd, String str){
+		this.command = command;
+		this.args = str;
+	}
+	
+	public Command getCommand(){
+		return this.command;
+	}
+	public String getArgs(){
+		return args;
+	}
+	
+}
